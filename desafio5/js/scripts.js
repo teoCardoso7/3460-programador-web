9a76acab5f7e522208549ee396122e76346fcf14
teoCardoso7 <teo.cardoso.7@gmail.com>
2018-07-07 14:43:35 -0300

    agregado part 2

--- desafio5/js/scripts.js
+++ desafio5/js/scripts.js
@@ -1,10 +1,13 @@
-function Student(name, lastName, dni, email) {
+
+
+var Student = function (name, lastName, dni, email) {
   //----------Public----------
 
   this.name = name;
   this.lastName = lastName;
   this.dni = dni;
   this.email = email;
+  var conocimientos = [];
   //-----------Private----------
   id = dni;
   //----------Function-----------
@@ -18,9 +21,37 @@ function Student(name, lastName, dni, email) {
       console.log("Mi nombre es " + name + " " + lastName + ".");
     }
   }
+
+this.addKnowledge = function (knowledge) {
+  conocimientos.push(knowledge);
+  console.log("El alumno " + name + " " + lastName + " ha aprendido " + knowledge);
+}
 }
 
 var student1 = new Student('Juan', 'Perez', '45678987','juan@gmail.com');
+var student2 = new Student('Juan', 'Perez2', '45678987','juan@gmail.com');
+var student3 = new Student('Juan', 'Perez3', '45678987','juan@gmail.com');
 
 student1.getFullName();
 student1.getID();
+//---------------Desafio parte 2------------------------
+var clase = [];
+clase.push(student1);
+clase.push(student2);
+clase.push(student3);
+
+var Profesor = function (nombre) {
+  this.nombre = nombre;
+
+  //-----------Functions----------
+  this.teach = function (knowledge) {
+    for (var i = 0; i < clase.length; i++)
+
+    clase[i].addKnowledge(knowledge);
+    }
+  }
+
+
+var profesor1 = new Profesor ('teo');
+
+profesor1.teach('lengua');
